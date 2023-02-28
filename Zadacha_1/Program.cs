551dd470f15a9b589c38db9496ad154030ae997b
Zotov Maksim <prolabor@bk.ru>
2023-02-28 10:57:16 +0300

    P1

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и 
+// выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int max;
+int min;
+if (a > b) { 
+    max = a;
+    min = b;
+} else {
+    max = b;
+    min = a;
+}
+Console.WriteLine("Максимальное значение: " + max);
+Console.WriteLine("Минимально значение: " + min);
+
+
---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if ( max < b) {
+    max = b;
+}
+if ( max < c ) {
+    max = c;
+}
+Console.WriteLine("Максимальное число: " + max);
